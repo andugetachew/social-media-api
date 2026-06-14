24e01525bae1f65284d4cab786142df1963bbeaa
Andualem Getachew <andugeta41@gmail.com>
2026-06-14 13:30:05 +0300

    feat: redirect root URL to api docs

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import permissions
-
+from django.views.generic import RedirectView
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/docs/"), name="root"),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
